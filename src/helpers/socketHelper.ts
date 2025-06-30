a7e52291d6946014e9ca195d7b3c5fd7ab29abde
Md Mohosin <mohosinali075@gmail.com>
2025-07-01 05:55:44 +0600

    real time location added

--- src/helpers/socketHelper.ts
+++ src/helpers/socketHelper.ts
@@ -6,6 +6,14 @@ const socket = (io: Server)=>{
     io.on('connection', socket=>{
         logger.info(colors.blue('A User connected'));
 
+
+        // for location testing
+        socket.on("sent-location", (data)=>{
+            logger.info(colors.green(`Location received: ${data}`));
+            // Emit the location data to all connected clients
+            io.emit("recieved-location", {id : socket.id, ...data});
+        });
+
         // disconnect
         socket.on("disconnect", ()=>{
             logger.info(colors.red('A user disconnect'));
