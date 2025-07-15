3744607510303c7912da86e394fdf0e3fe2212f7
Md Mohosin Ali <mohosinali075@gmail.com>
2025-07-15 06:12:04 +0600

    add football prediction

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import router from './routes';
 import { Morgan } from './shared/morgen';
 import './tasks/cronTasks';
+import axios from 'axios';
 const app = express();
 
 //morgan
@@ -12,15 +13,36 @@ app.use(Morgan.successHandler);
 app.use(Morgan.errorHandler);
 
 //body parser
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "*", // Use specific origin in production for security (e.g., "http://localhost:3000")
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 //file retrieve
 app.use(express.static('uploads'));
 
 //router
 app.use('/api/v1', router);
+
+// app.get("/api/football/teams", async (req, res) => {
+//   try {
+//     const response = await axios.get(
+//       "https://api.football-data.org/v4/competitions/PL/teams",
+//       {
+//         headers: {
+//           "X-Auth-Token": "8ed2a374d95c46a797a5ac55e7b37b6d"
+//         }
+//       }
+//     );
+//     res.json(response.data);
+//   } catch (err) {
+//     res.status(500).json({ error: "Error fetching data" });
+//   }
+// });
+
 
 //live response
 app.get('/', (req: Request, res: Response) => {
